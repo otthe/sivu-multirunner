ea2e35fea04f8ac02935b419b36e3211b0881324
ottoheiskanen <ottoheiskanen@gmail.com>
2026-04-08 16:20:25 +0300

    add tests for template req validations

--- test/server/site-handler.test.js
+++ test/server/site-handler.test.js
@@ -2,12 +2,22 @@ import { test, describe, it, expect, beforeEach, vi } from "vitest";
 import { resolveGetTemplatePath } from "../../src/utils/path.js";
 import { validatePublicTemplateRequest } from "../../src/server/site-handler.js";
 
-test("pretty url works", () => {
-  expect(resolveGetTemplatePath("/about", { allow_pretty_urls: true }))
-    .toBe("about.sivu");
+test("should throw error if not valid .sivu file", () => {
+  expect(() => validatePublicTemplateRequest("about"))
+    .toThrow("Not a sivu file");
 });
 
-// test("pretty url works", () => {
-//   expect(resolveGetTemplatePath("/about", { allow_pretty_urls: true }))
-//     .toBe("about.sivu");
-// });
+test("should throw error if not valid .sivu file", () => {
+  expect(() => validatePublicTemplateRequest("about"))
+    .toThrow("Not a sivu file");
+});
+
+test("should throw error if path contains private parts", () => {
+  expect(() => validatePublicTemplateRequest("users/_admin/secret.sivu"))
+    .toThrow("Private file not accessible");
+});
+
+test("should throw error if path contains private file parts", () => {
+  expect(() => validatePublicTemplateRequest("users/admin/_secret.sivu"))
+    .toThrow("Private file not accessible");
+});
