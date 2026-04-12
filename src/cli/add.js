e2318cb99b00b90746e149703612a6230c3261d2
ottoheiskanen <ottoheiskanen@gmail.com>
2026-04-12 11:53:16 +0300

    move internal req handler

--- src/cli/add.js
+++ src/cli/add.js
@@ -1,35 +1,4 @@
-import http from "node:http";
-
-const SOCKET_PATH = "/tmp/sivu.sock";
-
-function request(method, path, data) {
-  return new Promise((resolve, reject) => {
-    const req = http.request(
-      {
-        socketPath: SOCKET_PATH,
-        path,
-        method,
-        headers: {
-          "Content-Type": "application/json",
-        },
-      },
-      (res) => {
-        let body = "";
-
-        res.on("data", (chunk) => (body += chunk));
-        res.on("end", () => resolve(body));
-      }
-    );
-
-    req.on("error", reject);
-
-    if (data) {
-      req.write(JSON.stringify(data));
-    }
-
-    req.end();
-  });
-}
+import { request } from "../server/internal-handler.js";
 
 // simulate: sivu add test
 export async function run() {
--- src/server/internal-handler.js
+++ src/server/internal-handler.js
@@ -3,6 +3,38 @@ import path from "node:path";
 import crypto from "node:crypto";
 import fs from "node:fs";
 import express from "express";
+import http from "node:http";
+
+const SOCKET_PATH = "/tmp/sivu.sock";
+
+export function request(method, path, data) {
+  return new Promise((resolve, reject) => {
+    const req = http.request(
+      {
+        socketPath: SOCKET_PATH,
+        path,
+        method,
+        headers: {
+          "Content-Type": "application/json",
+        },
+      },
+      (res) => {
+        let body = "";
+
+        res.on("data", (chunk) => (body += chunk));
+        res.on("end", () => resolve(body));
+      }
+    );
+
+    req.on("error", reject);
+
+    if (data) {
+      req.write(JSON.stringify(data));
+    }
+
+    req.end();
+  });
+}
 
 export function createInternalHandler() {
   const router = express.Router();
